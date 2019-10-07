1fd0776341747829687112bd79ea7c31899d8793
Eduardo Silva <eduardo__lps@hotmail.com>
2019-10-07 15:26:10 +0100

    Added serial to waiter

--- src/agents/Waiter.java
+++ src/agents/Waiter.java
@@ -1,11 +1,14 @@
 package agents;
 
 import jade.core.Agent;
+import java.util.HashMap;
 
 public class Waiter extends Agent 
 {
+    private static final long serialVersionUID = 7110642579660810600L;
+
     protected void setup() 
-    {
+    {        
         // Printout a welcome message
         System.out.println("Sup");
     }
