6937923ef95ee4013690f2ea8ab4f12279703202
Eduardo Silva <eduardo__lps@hotmail.com>
2019-10-14 14:54:40 +0100

    feat: add yellow pages for waiter

--- src/agents/Waiter.java
+++ src/agents/Waiter.java
@@ -1,6 +1,10 @@
 package agents;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import utils.Kitchen;
 
 public class Waiter extends Agent 
@@ -10,6 +14,21 @@ public class Waiter extends Agent
     private static final long serialVersionUID = 7110642579660810600L;
 
     protected void setup() {        
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+
+        dfd.setName(this.getAID());
+        sd.setType("customer-service");
+        sd.setName("mas-restaurant");
+        dfd.addServices(sd);
+
+        try {
+            DFService.register(this, dfd);
+        }
+        catch(FIPAException e) {
+            e.printStackTrace();
+        }
+        
         System.out.println("Waiter " + this.getAID().getName() + " at your service.");
 
         if(kitchen == null)
@@ -19,6 +38,13 @@ public class Waiter extends Agent
     }
 
     protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        }
+        catch(FIPAException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Waiter " + this.getAID().getName() + " going home.");
     }
 }
