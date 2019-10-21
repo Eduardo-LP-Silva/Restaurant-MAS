f7280ab94643eaaac4dfd1609ac6aa38d879c4d0
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-10-21 15:38:15 +0100

    feat: add kitchen behaviour - TakeRequest
    Maybe it needs a better name.. Discussion needed.

---
+++ src/behaviours/TakeRequest.java
@@ -0,0 +1,26 @@
+package behaviours;
+
+import agents.Kitchen;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class TakeRequest extends CyclicBehaviour
+{
+    @Override
+    public void action() {
+        Kitchen myKitchen = (Kitchen) myAgent;
+        ACLMessage request = myAgent.receive();
+
+        if(request != null) {
+            ACLMessage reply = request.createReply();
+            String meal = request.getContent();
+
+            //TODO: Check if meal exists (if not, deny)
+            //TODO: Get information about meal (time + quality)
+            //TODO: Build reply with meal info
+            //TODO: Send reply
+        }
+        else
+            block();
+    }
+}
