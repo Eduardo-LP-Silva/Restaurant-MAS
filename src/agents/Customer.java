021647941485bfb684500d08f0acb219e4ef5f52
joanasmramos <joanasmramos98@gmail.com>
2019-10-28 17:51:12 +0000

    [FIX] Customer was sending repeated requests periodically

--- src/agents/Customer.java
+++ src/agents/Customer.java
@@ -11,25 +11,26 @@ public class Customer extends Agent {
     private static final long serialVersionUID = 3921787877132989337L;
     private String desiredDish;
     private AID[] waiters = null;
+    private boolean hasStartedOrder = false;
     private int mood;
 
     @Override
     protected void setup() {
         Object[] args = getArguments();
         
-        System.out.println("(customer) Hello! Customer " + getAID().getLocalName() + " is ready.");
+        printMessage("Hello! Customer " + getAID().getLocalName() + " is ready.");
 
         if (args != null && args.length == 1) {
             desiredDish = (String) args[0];
             // standardizing dish name
             desiredDish =  desiredDish.replace("-", " ");
             desiredDish = desiredDish.toLowerCase();
-            System.out.println("(customer) I want to eat " + desiredDish + "!");
+            printMessage("I want to eat " + desiredDish + "!");
 
             Random random = new Random();
             mood = random.nextInt(9) + 1; //10 being very relaxed and 1 being very frustrated
         } else {
-            System.out.println("(customer) No dish specified!");
+            printMessage("No dish specified!");
             doDelete();
         }
 
@@ -48,12 +49,19 @@ public class Customer extends Agent {
         return desiredDish;
     }
 
-    @Override
-    protected void takeDown() {
-        System.out.println("(customer) Customer " + getAID().getLocalName() + " is going home.");
+    public void startOrder() {
+        if(!hasStartedOrder) {
+            addBehaviour(new OrderPerformer(this));
+            hasStartedOrder = true;
+        }
     }
 
-    public void startOrder() {
-        // addBehaviour(new OrderPerformer(this));
+    public void printMessage(String message) {
+        System.out.println("(Customer " + getAID().getLocalName() + ") " + message);
+    }
+
+    @Override
+    protected void takeDown() {
+        printMessage("Going home");
     }
 }
--- src/behaviours/OrderPerformer.java
+++ src/behaviours/OrderPerformer.java
@@ -3,7 +3,11 @@ package behaviours;
 import agents.Customer;
 import jade.core.AID;
 import jade.core.behaviours.BaseInitiator;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.DataStore;
 import jade.lang.acl.ACLMessage;
+
+import java.util.concurrent.TimeUnit;
 
 public class OrderPerformer extends BaseInitiator {
     private static final long serialVersionUID = 2897989135282380056L;
@@ -17,14 +21,21 @@ public class OrderPerformer extends BaseInitiator {
 
     @Override
     protected void handleAgree(ACLMessage agree) {
-        System.out.println("(customer) Waiting for my dish.");
+        customer.printMessage("Waiting for my dish.");
     }
 
     @Override
     protected ACLMessage createInitiation() {
+        /*
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        */
+
         ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
         msg.addReceiver(currentWaiter);
-        msg.setLanguage("English");
         msg.setConversationId("waiter-request");
         msg.setContent(customer.getDesiredDish());
         customer.send(msg);
